02eb37172b3e44cc00c07b665d9159ab51a360c6
nehabpandya <neha@sswhite.net>
2025-11-09 13:20:39 +0530

    Feature: v1.2.1 - Add tabs, charts, and progress tracker

--- ACEest_Fitness.py
+++ ACEest_Fitness.py
@@ -245,4 +245,5 @@ def progress():
 
 # 11. --- RUN THE APP ---
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)git status
+    
